0da0bc91d80e4c2c5561cb211bb7f3835a3a809c
nishantprime <nishantkumarprime@gmail.com>
2024-12-10 21:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from helper import get_page_links
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST']):
-  def home():
-    if request.method == 'GET':
-      return render_template('index.html')
-    else:
-      url = request.form['URL']
-      links = get_page_links(url)
-      return render_template('index.html', links=links)
-
-
-
+    def home():
+        if request.method == 'GET':
+            return render_template('index.html')
+        else:
+            url = request.form['URL']
+            links = get_page_links(url)
+            return render_template('index.html', links=links)
+if __name__ == '__main__':
+    app.run
