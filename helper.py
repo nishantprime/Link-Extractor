4aa2db0a29e1f865eeddbfa8577f5dfd1e3f042d
nishantprime <nishantkumarprime@gmail.com>
2024-12-10 22:03:59 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,28 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 }
-
 
 def get_page_links(url):
   if url.endswith('/'):
     url = url[:-1]
   response = requests.get(url, headers = headers)
   soup = BeautifulSoup(response.text, 'html.parser')
+  raw = [link['href'] for link in soup.find_all('a', href = True)]
   links = []
-  raw = soup.find_all('a', href = True)
   for link in raw:
-    link = link.get('href')
     if link.startswith('/'):
       link = url+link
     links.append(link)
   
   return links
-
