db7f819bd92b8babf9031354f39a5f987bac7ad8
nishantprime <nishantkumarprime@gmail.com>
2024-12-10 21:25:47 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+}
+
+
+def get_page_links(url):
+  if url.endswith('/'):
+    url = url[:-1]
+  response = requests.get(url, headers = headers)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  links = []
+
+  for link in soup.find_all('a', href = True):
+    if link.startswith('/'):
+      link = url+link
+    links.append(link)
+  
+  return links
+
