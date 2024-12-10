fd0479eefdaae5e2447218caf4a48b41bbf79175
nishantprime <nishantkumarprime@gmail.com>
2024-12-10 23:05:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def home():
             url = request.form['URL']
             links = get_page_links(url)
             if links == 0:
-                return render_template('index.html', error = 'Site not accessable')
+                return render_template('index.html', error = f'Site Not Accessable - {url}')
             if not links:
-                return render_template('index.html', error = 'No Links Found')
+                return render_template('index.html', error = f'No Links Found on {url}')
             links.insert(0,url)
             return render_template('index.html', links=links)
         except Exception as e:
-            return render_template('index.html', error = str(e))
+            return render_template('index.html', error = f'{str(e)} on {url}')
 if __name__ == '__main__':
     app.run(debug=True)
